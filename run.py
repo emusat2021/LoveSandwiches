e57ada5618e30db54e40f67db599cf541a6289a7
Eugenia Musat <musat.eugenia@gmail.com>
2021-06-23 11:34:09 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -28,6 +28,9 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
+    
+    """
+    #a
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
@@ -37,9 +40,27 @@ def get_sales_data():
     sales_data = data_str.split(",") #split() method returns the broken up values as a list
     # print(sales_data)
     validate_data(sales_data) # calling the functin
+    """
+    #b repeat request/continue to repeate
+
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break # the code will repeate it self until valid data and then break
+
+    return sales_data
+
 
 def validate_data(values):
-    # print(values)
+    print(values)
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
@@ -49,14 +70,22 @@ def validate_data(values):
     can be converted into integers)
     """
     try:
-        if len(values) != 6:
+        [int(value) for value in values] #2
+        if len(values) != 6: #1
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        #b
+        return False
+    #b
+    return True 
 
-get_sales_data()
+
+#b
+data = get_sales_data()
+# get_sales_data()
 
 
 
